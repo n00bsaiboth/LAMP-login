9dfab151c24c24ed32123b6b5a21e0510e472bca
Jussi Jokinen <jussi.k.jokinen@utu.fi>
2021-04-20 13:23:24 +0300

    Updated the header and the index file.

--- __/php/header.php
+++ __/php/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
 
-    <title>PHP-MySQL Login</title>
+    <title>LAMP-login</title>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
@@ -12,3 +12,36 @@
 </head>
 
 <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">LAMP-login</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+            <?php 
+                if(isset($_SESSION["id"]) && !empty($_SESSION["id"])) {  
+                    echo "<a class=\"nav-link\" href=\"" . validateURL("logout.php") . "\">Logout</a>";
+                } else {
+                    echo "<a class=\"nav-link\" href=\"" . validateURL("login.php") . "\">Login</a>";
+                }
+            ?>  
+            </li>
+            <li class="nav-item">
+            <?php 
+                if(empty($_SESSION["id"])) {  
+                    echo "<a class=\"nav-link\" href=\"" . validateURL("register.php") . "\">Register</a>";
+                } 
+            ?>         
+            </li>
+            <li class="nav-item">
+            <?php 
+                if(isset($_SESSION["id"]) && !empty($_SESSION["id"])) {  
+                    echo "<a class=\"nav-link\" href=\"" . validateURL("profile.php") . "\">Profile</a>";
+                } 
+            ?>         
+            </li>
+        </div>
+    </nav>
--- index.php
+++ index.php
@@ -8,39 +8,6 @@
 
 ?>
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">PHP-MySQL Login</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-        <?php 
-            if(isset($_SESSION["id"]) && !empty($_SESSION["id"])) {  
-                echo "<a class=\"nav-link\" href=\"" . validateURL("logout.php") . "\">Logout</a>";
-            } else {
-                echo "<a class=\"nav-link\" href=\"" . validateURL("login.php") . "\">Login</a>";
-            }
-        ?>  
-        </li>
-        <li class="nav-item">
-        <?php 
-            if(empty($_SESSION["id"])) {  
-                echo "<a class=\"nav-link\" href=\"" . validateURL("register.php") . "\">Register</a>";
-            } 
-        ?>         
-        </li>
-        <li class="nav-item">
-        <?php 
-            if(isset($_SESSION["id"]) && !empty($_SESSION["id"])) {  
-                echo "<a class=\"nav-link\" href=\"" . validateURL("profile.php") . "\">Profile</a>";
-            } 
-        ?>         
-        </li>
-    </div>
-    </nav>
     <section class="container" id="welcome">
         <div class="jumbotron">
             <h1 class="display-4">Welcome to PHP-MySQL Login</h1>
