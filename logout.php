f00d2ecc0c5a1a2171b094fc4079caf917a1c963
Jussi Jokinen <jussi.k.jokinen@utu.fi>
2021-04-20 11:37:36 +0300

    Updated the logout.

--- logout.php
+++ logout.php
@@ -1,10 +1,20 @@
 <?php
     session_start();
-    
-    $_SESSION['id'] = "";
-    $_SESSION["error"] = "";
+
+    // Unset session variables
+    // $_SESSION = array();
+
+    session_unset(); 
+ 
+    // Destroy the session
+
+    session_destroy();
+
+    // If session was destroyed, then redirect to homepage. 
    
-    if(empty($_SESSION['id'])) {
+    if(empty($_SESSION)) {
         header("location: index.php");
+    } else {
+        header("location: profile.php");
     }
 ?>
